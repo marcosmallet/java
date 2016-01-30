cfc507a78abd42d9a8ce9ec650f89cb12c9d70ee
marcosmallet <marcosmallet@gmail.com>
2016-01-30 18:00:50 -0200

    login 3

--- src/main/resources/public/js/login.js
+++ src/main/resources/public/js/login.js
@@ -7,15 +7,12 @@ $(document).ready(function(){
 			url:"https://nameless-cove-36890.herokuapp.com/login", //REST Java
 			type:"post",				//Método de envio
 			data: "email="+email+"&password="+password,	//Dados
-   			success: function (result){
-   						//Sucesso no AJAX
-   						console.dir(result);
-                		if(result==1){						
-                			location.href='lista.php'	//Redireciona
-                		}else{
-                			$('#errolog').show();		//Informa o erro
-                		}
-            		}
+   			success: function (result){ //Sucesso no AJAX					
+                location.href='lista.php'	//Redireciona
+            },
+            error: function(result) {
+                $('#errolog').show();
+            }
 		})
 		return false;	//Evita que a página seja atualizada
 	})
