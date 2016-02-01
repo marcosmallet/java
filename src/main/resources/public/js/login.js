633532ee878a2ceea389f9a771acb34636316f28
marcosmallet <marcosmallet@gmail.com>
2016-02-01 08:21:06 -0200

    correcoes

--- src/main/resources/public/js/login.js
+++ src/main/resources/public/js/login.js
@@ -1,19 +1,20 @@
 $(document).ready(function(){
-	$('#errolog').hide(); //Esconde o elemento com id errolog
+	$('#errorlog').hide(); //Esconde o elemento com id errorlog
+	$('#successlog').hide(); //Esconde o elemento com id successlog
 	$('#formlogin').submit(function(){ 	//Ao submeter formulário
 		var email=$('#email').val();	//Pega valor do campo email
 		var password=$('#password').val();	//Pega valor do campo senha
 		$.ajax({			//Função AJAX
-			url:"https://nameless-cove-36890.herokuapp.com/login", //REST Java
+			url:"https://tecvidya-java.herokuapp.com/login", //REST Java
 			type:"post",				//Método de envio
 			data: "email="+email+"&password="+password,	//Dados
    			success: function (result){ //Sucesso no AJAX					
-                location.href='lista.php'	//Redireciona
+                $('#successlog').show();
             },
             error: function(result) {
                 $('#errolog').show();
             }
 		})
-		return false;	//Evita que a página seja atualizada
+		return false;
 	})
 })
